48043007973c4199b45e0f326729702863517ea9
alekseyredko <aliakseiredzko@gmail.com>
2021-07-30 14:08:59 +0300

    Create BlazorWebServerAuthExtensions.cs

---
+++ Extensions/BlazorWebServerAuthExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Infrastructure.Services.Interfaces;
+using Movies.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Infrastructure.Extensions
+{
+    public static class BlazorWebServerAuthExtensions
+    {
+        public static void AddBlazorWebServerAuth(this IServiceCollection services)
+        {
+            services.AddScoped<ServerAuthenticationStateProvider, UserAuthenticationStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(provider =>
+                provider.GetRequiredService<ServerAuthenticationStateProvider>());
+
+            services.AddScoped<ICustomAuthentication, CustomAuthentication>();
+        }
+    }
+}
