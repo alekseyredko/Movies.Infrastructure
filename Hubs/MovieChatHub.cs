87d45c34c65d89858b84165a14fdeab258825058
alekseyredko <aliakseiredzko@gmail.com>
2021-08-02 17:10:12 +0300

    add groups

--- Hubs/MovieChatHub.cs
+++ Hubs/MovieChatHub.cs
@@ -10,9 +10,20 @@ namespace Movies.Infrastructure.Hubs
 {
     public class MovieChatHub: Hub
     {
-        public async Task SendMessageAsync(ChatMessageRequest request)
+        public async Task SendMessageAsync(ChatMessageRequest request, string groupName)
         {
-            await Clients.All.SendAsync("ReceiveMessageAsync", request);
+            await Clients.Group(groupName).SendAsync("ReceiveMessageAsync", request);
+        }
+
+        public async Task AddUserToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+        
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
         }
     }
 }
